5438d2befc8b7631a8645d434bc84ac5b44deab8
李明辉 <liminghui-khjy@bestpay.com.cn>
2020-08-26 12:42:26 +0800

    修改代码格式

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,25 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 Vue.use(Router)
-const AnimalContainer = resolve => require(['../AnimalContainer.vue'], resolve)
-const AnimalFlow = resolve => require(['../AnimalFlow.vue'], resolve)
+const AnimalContainer = (resolve) => require(['../AnimalContainer.vue'], resolve)
+const AnimalFlow = (resolve) => require(['../AnimalFlow.vue'], resolve)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'animal',
-      component: AnimalContainer,
-      children: [
+    routes: [
         {
-          path: '/',
-          redirect: 'animal-flow'
-        },
-        {
-          path: 'animal-flow',
-          name: 'animal-flow',
-          component: AnimalFlow
+            path: '',
+            component: AnimalContainer,
+            children: [
+                {
+                    path: '/',
+                    redirect: 'animal-flow'
+                },
+                {
+                    path: 'animal-flow',
+                    name: 'animal-flow',
+                    component: AnimalFlow
+                }
+            ]
         }
-      ]
-    }
-  ]
+    ]
 })
